c7b42f6c1a9fe2c363a2e30fc5eb649c115ba6eb
trieu1994 <nguyenquangtrieu1994@gmail.com>
2018-06-02 14:54:12 +0700

    asd

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-
+console.log(__dirname);
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.render('index.html');
 });
 
 // Start the app by listening on the default Heroku port
