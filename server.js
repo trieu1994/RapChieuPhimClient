bdb0d51886ba15ee1e389903927f077c8026d4a5
trieu1994 <nguyenquangtrieu1994@gmail.com>
2018-06-02 14:57:52 +0700

    fix23

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/src'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/src/index.html'));
+    res.render('index.html');
 });
 
 // Start the app by listening on the default Heroku port
