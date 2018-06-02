f99229a18cc39a9729084a46732decab904de1a9
trieu1994 <nguyenquangtrieu1994@gmail.com>
2018-06-02 15:28:23 +0700

    server.js fix 2

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-//Install express server
+// server.js
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
+// Run the app by serving the static files
+// in the dist directory
 app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req,res) {
-    
-    res.render('index.html');
-});
-
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
