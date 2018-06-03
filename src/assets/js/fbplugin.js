c9671efa9bf9e8d8f73ddcfeda3281a3765ff085
trieu1994 <nguyenquangtrieu1994@gmail.com>
2018-06-03 09:18:41 +0700

    fix cm

--- src/assets/js/fbplugin.js
+++ src/assets/js/fbplugin.js
@@ -37,25 +37,25 @@
 //      js.src = "//connect.facebook.net/<lan_LAN>/sdk.js";
 //      fjs.parentNode.insertBefore(js, fjs);
 //    }(document, 'script', 'facebook-jssdk'));
-window.fbAsyncInit = function () {
-        FB.init({
-            appId: '1650438311721724',
-            cookie: true,
-            xfbml: true,
-            version: 'v3.0',
-            status: true
-        });
-        FB.AppEvents.logPageView();
-        $('body').trigger('facebook:init');
-    };
+// window.fbAsyncInit = function () {
+//         FB.init({
+//             appId: '1650438311721724',
+//             cookie: true,
+//             xfbml: true,
+//             version: 'v3.0',
+//             status: true
+//         });
+//         FB.AppEvents.logPageView();
+//         $('body').trigger('facebook:init');
+//     };
 
-    // Load the SDK asynchronously
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id))
-            return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+//     // Load the SDK asynchronously
+//     (function (d, s, id) {
+//         var js, fjs = d.getElementsByTagName(s)[0];
+//         if (d.getElementById(id))
+//             return;
+//         js = d.createElement(s);
+//         js.id = id;
+//         js.src = "//connect.facebook.net/en_US/sdk.js";
+//         fjs.parentNode.insertBefore(js, fjs);
+//     }(document, 'script', 'facebook-jssdk'));
